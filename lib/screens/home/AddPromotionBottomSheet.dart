fa62d873c1c686faf9cccc7e5708f7f4dd396bd7
andryue <kacperwojak17@gmail.com>
2020-06-29 12:48:37 +0200

    aout band pixels bug fixing

--- lib/screens/home/AddPromotionBottomSheet.dart
+++ lib/screens/home/AddPromotionBottomSheet.dart
@@ -11,100 +11,102 @@ class AddPromotionBottomSheet extends StatefulWidget {
 class _AddPromotionBottomSheetState extends State<AddPromotionBottomSheet> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-      child: Form(
-        child: Column(
-          children: <Widget>[
-            Text(
-              "Add promotion",
-              style: TextStyle(
-                  fontSize: 24.0,
-                  color: Colors.grey[900],
-                  fontFamily: "mont",
-                  fontWeight: FontWeight.w600),
-            ),
-            Text(
-              "Add product information",
-              style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.grey[700],
-                  fontFamily: "mont",
-                  fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            TextFormField(
-              style: regular_text,
-              decoration: little_radius_border.copyWith(
-                  hintText: "Product name",
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 25.0,
-                    vertical: 22.0,
-                  )),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            TextFormField(
-              style: regular_text,
-              decoration: little_radius_border.copyWith(
-                  hintText: "Product image URL",
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 25.0,
-                    vertical: 22.0,
-                  )),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            TextFormField(
-              style: regular_text,
-              decoration: little_radius_border.copyWith(
-                  hintText: "Shop",
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 25.0,
-                    vertical: 22.0,
-                  )),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            TextFormField(
-              style: regular_text,
-              decoration: little_radius_border.copyWith(
-                  hintText: "Price",
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 25.0,
-                    vertical: 22.0,
-                  )),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: FlatButton(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-                    onPressed: () {},
-                    child: Text(
-                      "Add new product",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18.0,
-                          fontFamily: "mont",
-                          fontWeight: FontWeight.w600),
-                    ),
-                    color: Colors.amber,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7.0),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Add promotion",
+                style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.grey[900],
+                    fontFamily: "mont",
+                    fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "Add product information",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey[700],
+                    fontFamily: "mont",
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              TextFormField(
+                style: regular_text,
+                decoration: little_radius_border.copyWith(
+                    hintText: "Product name",
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 22.0,
+                    )),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                style: regular_text,
+                decoration: little_radius_border.copyWith(
+                    hintText: "Product image URL",
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 22.0,
+                    )),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                style: regular_text,
+                decoration: little_radius_border.copyWith(
+                    hintText: "Shop",
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 22.0,
+                    )),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                style: regular_text,
+                decoration: little_radius_border.copyWith(
+                    hintText: "Price",
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 25.0,
+                      vertical: 22.0,
+                    )),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                      onPressed: () {},
+                      child: Text(
+                        "Add new product",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontFamily: "mont",
+                            fontWeight: FontWeight.w600),
+                      ),
+                      color: Colors.amber,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7.0),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
